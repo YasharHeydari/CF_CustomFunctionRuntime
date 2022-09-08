eff238cee8457bc1e41ae1b8edaa08c3688c4b5d
Yashar.Heydari <yashar.heydari@canalyst.com>
2022-09-08 14:34:13 -0700

    Custom Function Runtime

---
+++ src/functions/functions.ts
@@ -0,0 +1,12 @@
+﻿/* global clearInterval, console, CustomFunctions, setInterval */
+
+/**
+ * Adds two numbers.
+ * @customfunction
+ * @param first First number
+ * @param second Second number
+ * @returns The sum of the two numbers.
+ */
+export function add(first: number, second: number): number {
+  return first + second;
+}
